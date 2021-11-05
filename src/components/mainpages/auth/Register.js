682813d30a669b71322aac38499968d4a29846e8
MEAN Hive <meanhive@zoho.com>
2021-11-05 14:04:27 +0530

    Update Register.js

--- src/components/mainpages/auth/Register.js
+++ src/components/mainpages/auth/Register.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 import { toast } from 'react-toastify';
-import config from '../../config.json'
+import config from '../../../config.json'
 
 export default function Register() {
     // const [location, setLocation] = useState({});
