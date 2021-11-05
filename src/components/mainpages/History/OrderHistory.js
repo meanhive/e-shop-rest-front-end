0466326b8ea8578bba7609f9e4b786d23108b7b5
MEAN Hive <meanhive@zoho.com>
2021-11-05 13:58:49 +0530

    Update OrderHistory.js

--- src/components/mainpages/History/OrderHistory.js
+++ src/components/mainpages/History/OrderHistory.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import { GlobalState } from '../../../GlobalState';
 import { Link } from 'react-router-dom'
-import config from '../../config'
+import config from '../../config.json'
 
 export default function OrderHistory() {
 	const state = useContext(GlobalState);
