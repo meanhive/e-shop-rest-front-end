24d0881bb29b82ea6b56cae83b89461c5c8b8039
MEAN Hive <meanhive@zoho.com>
2021-11-05 14:04:15 +0530

    Update Login.js

--- src/components/mainpages/auth/Login.js
+++ src/components/mainpages/auth/Login.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { toast } from 'react-toastify';
-import config from '../../config.json'
+import config from '../../../config.json'
 
 export default function Login() {
     const [user, setUser] = useState({
